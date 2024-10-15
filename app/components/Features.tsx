6c1bdf334fa8ad4a546e1dfb37480f999983a9ff
pastuchenko <pablopastuchenkooo@gmail.com>
2024-10-15 08:30:44 -0300

    first commit

---
+++ app/components/Features.tsx
@@ -0,0 +1,160 @@
+import Image from "next/image";
+import Feature1 from "../../public/assets/feature-1.svg";
+import Feature2 from "../../public/assets/feature-2.svg";
+import Feature3 from "../../public/assets/feature-3.svg";
+import Check from "../../public/assets/check.svg";
+import bluebutton from "../../public/assets/blue-button.svg";
+import greenButton from "../../public/assets/green-button.svg";
+import pinkButton from "../../public/assets/pink-button.svg";
+
+export function Features() {
+  return (
+    <div className="flex flex-col gap-y-[56px] py-[56px] lg:py-[120px] lg:gap-y-[80px] ">
+      <div className="flex flex-col gap-x-6 sm:flex-row-reverse">
+        <Image
+          src={Feature1}
+          alt="Imagem da Feature 1"
+          className="hidden w-1/2 sm:block"
+        />
+        <div className="sm:w-1/2 lg:py-[56px] lg:pr-[56px]">
+          <h3 className="font-medium text-[#0085FF] lg:text-[18px] ">
+            Monitoramento de Vendas
+          </h3>
+          <h1 className="pt-[12px] text-2xl font-medium text-[#172026] lg:text-[42px] lg:leading-[58px]">
+            Simplifique seu monitoramento de vendas
+          </h1>
+          <Image
+            src={Feature1}
+            alt="Imagem da Feature 1"
+            className="pt-[24px] sm:hidden"
+          />
+          <p className="py-[24px] text-[#36485C] lg:text-[18px]">
+            Fique no controle e renove seu processo de trabalho com nosso recurso inovador. Tenha uma visão panorâmica com nosso painel personalizável.
+          </p>
+
+          <ul className="flex flex-col gap-y-3 lg:text-[18px]">
+            <li className="flex items-center gap-x-2 text-[#36485C]">
+              <span>
+                <Image src={Check} alt="Marca de Verificação" />
+              </span>
+              Lorem ipsum dolor sit amet
+            </li>
+            <li className="flex items-center gap-x-2 text-[#36485C]">
+              <span>
+                <Image src={Check} alt="Marca de Verificação" />
+              </span>
+              Lorem ipsum dolor sit amet
+            </li>
+            <li className="flex items-center gap-x-2 text-[#36485C]">
+              <span>
+                <Image src={Check} alt="Marca de Verificação" />
+              </span>
+              Lorem ipsum dolor sit amet
+            </li>
+          </ul>
+
+          <p className="flex items-center gap-x-2 pt-[24px] font-medium text-[#0085FF] lg:text-[18px]">
+            Saiba Mais{" "}
+            <span>
+              <Image src={bluebutton} alt="Saiba mais" />
+            </span>
+          </p>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-x-6 sm:flex-row">
+        <Image
+          src={Feature2}
+          alt="Imagem da Feature 2"
+          className="hidden w-1/2 sm:block"
+        />
+        <div className="sm:w-1/2 lg:py-[56px] lg:pl-[56px]">
+          <h3 className="font-medium text-[#00A424] lg:text-[18px] ">
+            Suporte ao Cliente
+          </h3>
+          <h1 className="pt-[12px] text-2xl font-medium text-[#172026] lg:text-[42px] lg:leading-[58px]">
+            Conecte-se com seus clientes
+          </h1>
+          <Image
+            src={Feature2}
+            alt="Imagem da Feature 2"
+            className="pt-[24px] sm:hidden"
+          />
+          <p className="py-[24px] text-[#36485C] lg:text-[18px]">
+            Fique no controle e renove seu processo de trabalho com nosso recurso inovador. Tenha uma visão panorâmica com nosso painel personalizável.
+          </p>
+
+          <ul className="flex flex-col gap-y-3 lg:text-[18px]">
+            <li className="flex items-center gap-x-2 text-[#36485C]">
+              <span>
+                <Image src={Check} alt="Marca de Verificação" />
+              </span>
+              Lorem ipsum dolor sit amet
+            </li>
+            <li className="flex items-center gap-x-2 text-[#36485C]">
+              <span>
+                <Image src={Check} alt="Marca de Verificação" />
+              </span>
+              Lorem ipsum dolor sit amet
+            </li>
+            <li className="flex items-center gap-x-2 text-[#36485C]">
+              <span>
+                <Image src={Check} alt="Marca de Verificação" />
+              </span>
+              Lorem ipsum dolor sit amet
+            </li>
+          </ul>
+
+          <p className="flex items-center gap-x-2 pt-[24px] font-medium text-[#00A424] lg:text-[18px]">
+            Saiba Mais{" "}
+            <span>
+              <Image src={greenButton} alt="Saiba mais" />
+            </span>
+          </p>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-x-6 sm:flex-row-reverse">
+        <Image
+          src={Feature3}
+          alt="Imagem da Feature 3"
+          className="hidden w-1/2 sm:block"
+        />
+        <div className="sm:w-1/2 lg:py-[56px] lg:pr-[56px]">
+          <h3 className="font-medium text-[#EB2891] lg:text-[18px] ">
+            Monitoramento de Crescimento
+          </h3>
+          <h1 className="pt-[12px] text-2xl font-medium text-[#172026] lg:text-[42px] lg:leading-[58px]">
+            Monitore novos assinantes do seu site
+          </h1>
+          <Image
+            src={Feature3}
+            alt="Imagem da Feature 3"
+            className="pt-[24px] sm:hidden"
+          />
+          <p className="py-[24px] text-[#36485C] lg:text-[18px]">
+            Fique no controle e renove seu processo de trabalho com nosso recurso inovador. Tenha uma visão panorâmica com nosso painel personalizável.
+          </p>
+
+          <div className="flex w-full gap-x-[24px] ">
+            <div className="w-1/2 flex flex-col gap-y-3">
+              <h3 className="text-[20px] font-medium text-[#172026]">100+</h3>
+              <p className="text-[#36485C] ">Lorem ipsum dolor sit</p>
+            </div>
+            <div className="w-1/2 flex flex-col gap-y-3">
+              <h3 className="text-[20px] font-medium text-[#172026]">800+</h3>
+              <p className="text-[#36485C]">Conse adipiscing elit</p>
+            </div>
+          </div>
+
+          <p className="flex items-center gap-x-2 pt-[24px] font-medium text-[#EB2891] lg:text-[18px]">
+            Saiba Mais{" "}
+            <span>
+              <Image src={pinkButton} alt="Saiba mais" />
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
